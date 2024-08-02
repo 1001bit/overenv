39df966d03f231fc54c900a49d933dd862332703
Schalmanoff <yaribakal@gmail.com>
2024-08-02 11:11:09 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Simple library easily manage environmental variables
+
+## Import the module into your project:
+`go get github.com/1001bit/overenv`
---
+++ overenv.go
@@ -0,0 +1,44 @@
+package overenv
+
+import (
+	"fmt"
+	"os"
+	"reflect"
+
+	"github.com/joho/godotenv"
+)
+
+const tagName = "env"
+
+func DotEnvLoad() {
+	godotenv.Load()
+}
+
+func Get(key string) string {
+	return os.Getenv(key)
+}
+
+func LoadStruct(str any) error {
+	t := reflect.TypeOf(str).Elem()
+
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		if !field.IsExported() {
+			continue
+		}
+
+		if tag := field.Tag.Get(tagName); tag != "" {
+			envVarValue := Get(tag)
+
+			switch field.Type.Kind() {
+			case reflect.String:
+				strVal := reflect.ValueOf(str).Elem().FieldByName(field.Name)
+				strVal.SetString(envVarValue)
+			default:
+				return fmt.Errorf("unsupported type %s for field %s", field.Type, field.Name)
+			}
+		}
+	}
+
+	return nil
+}
